a9622c2f914be2b94173cb374a541784abee4717
itsparasbisht <iamparasbisht@gmail.com>
2023-03-14 19:45:09 +0530

    loading file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>hello</h1>
+      <Link href="/about">Go to about</Link>
     </main>
   );
 }
