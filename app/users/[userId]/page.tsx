f7e98514e03aa3769b5d995e08419146095bd9d3
itsparasbisht <iamparasbisht@gmail.com>
2023-03-19 16:32:14 +0530

    UserPosts component as a server component

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,5 +1,7 @@
 import getUser from "@/lib/getUser";
 import getUserPosts from "@/lib/getUserPosts";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
 
 type Params = {
   params: {
@@ -11,10 +13,15 @@ export default async function UserPage({ params: { userId } }: Params) {
   const userData: Promise<User> = getUser(userId);
   const userPostsData: Promise<Post[]> = getUserPosts(userId);
 
-  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  const user = await userData;
   return (
     <>
       <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>loading...</h2>}>
+        {/* @ts-expect-error Async Server Component */}
+        <UserPosts promise={userPostsData} />
+      </Suspense>
     </>
   );
 }
