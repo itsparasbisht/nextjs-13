425e781c9710ff28dc95b257160ce44dca8535a7
itsparasbisht <iamparasbisht@gmail.com>
2023-05-13 13:59:17 +0530

    changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,17 @@
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <h1>About our team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quod
+        atque similique animi quo quaerat nesciunt laborum doloremque, magnam
+        laboriosam amet. Asperiores consequuntur neque nam repudiandae quas ea,
+        nostrum nobis, veritatis porro sequi deleniti, rerum minima vitae!
+        Dignissimos optio magni eligendi voluptatum corporis pariatur sequi
+        repudiandae unde, debitis maiores et.
+      </p>
+    </div>
+  );
 };
 
 export default AboutPage;
